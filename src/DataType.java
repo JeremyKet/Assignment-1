9385ef654fc6695b6e066a1e9ca736c9f65626ac
JeremyKet <jeremyketema@gmail.com>
2020-06-15 17:45:45 -0400

    message

---
+++ src/DataType.java
@@ -0,0 +1,23 @@
+public class DataType {
+    public static void main(String[] args) {
+        String patientName = "Stacey Manson";
+        boolean hasInsurance=false;
+        double temp=102.2;
+        int numVisits=5;
+        float copayAmount=10000;
+        String todayDate = "06/15/2020";
+        String phoneNumber = "3018005555";
+
+        System.out.println("Patient Details:");
+        System.out.println("Name:"+patientName);
+        System.out.println("Patient has insurance:"+hasInsurance);
+        System.out.println("Body temprature:"+temp);
+        System.out.println("Number of visits:"+numVisits);
+        System.out.println("Next appointment:"+todayDate);
+        System.out.println("Copay Amount:"+copayAmount);
+        System.out.println("Phone number:"+phoneNumber);
+
+
+
+    }
+}
